8f3ff199137161d4b8d85155d6b15211b49e03b4
Kramarina12 <kravtsova.0404@gmail.com>
2024-09-16 20:06:43 +0000

    Добавлен файл info_text.sh

---
+++ lab1/src/info_text.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+echo "Текущий путь: /home/runner/oslab2019-1/lab1/src"
+echo "Текущая дата и время: Mon 16 Sep 2024 08:04:19 PM UTC"
+echo "Содержимое переменной окружения PATH: /nix/store/vz80by8cnidi68zd47dw6c5clrkdq3f7-clang-wrapper-14.0.6/bin:/nix/store/s71vlhlz1kmkix09xcdkia291dmvgcv3-pid1/bin:/nix/store/ky4aqzdmv0j0rrbw14hr0xmsrf0amssl-replit-runtime-path/bin:/home/runner/.nix-profile/bin:/home/runner/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
